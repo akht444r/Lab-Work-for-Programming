5e99cdadc29a968e621932c59291854047453e27
akht444r <akhtargadangabimanyu@mail.ugm.ac.id>
2024-09-23 18:10:00 +0700

    Update and rename sum to sum.cpp

---
+++ Week_4/Problem_1/sum.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    int num, sum = 0;               //Declare variables for number and sum
+    int i;                          //Declare variable for i
+    cout << "Input: ";              //Ask user for input
+    cin >> num;                     
+    for (i=1; i<=num; i++){         //Set the starting number, limit of i
+        sum+=i;                     //Add for every numbers
+    }
+    cout << "Numbers: ";            //Show numbers in the range of num
+    for (i=1; i<=num; i++){
+        cout << i << " + ";         //Show numbers and '+' symbol
+    }
+    cout << endl;
+cout << "Result: " << sum;          //Show sum result
+
+ return 0;
+}
