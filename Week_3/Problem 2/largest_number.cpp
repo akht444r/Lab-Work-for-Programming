8d1385f44e0bc691872240a80b0e45f4ec2ffc85
akht444r <akhtargadangabimanyu@mail.ugm.ac.id>
2024-09-24 15:43:59 +0700

    Update largest_number.cpp

--- Week_3/Problem 2/largest_number.cpp
+++ Week_3/Problem 2/largest_number.cpp
@@ -14,18 +14,19 @@ int main() {
     cout << "Input number 3: ";                                          //Ask user for number
     cin >> num3;                                                         //User input random number
     
-    if (int largest = num1) {                                            // Initialize 'largest' with the value of 'num1' and check if 'num1' is the largest number
+if (int largest = num1) {                                            // Initialize 'largest' with the value of 'num1' and check if 'num1' is the largest number
     cout << "The largest number is " << largest << endl;                 // Print the largest number if 'num1' is the largest
-}
+    }
 
 else if (num2 > largest) {                                               // Check if 'num2' is greater than the current 'largest'
     largest = num2;                                                      // If true, assign 'num2' as the largest
-}
+    }
 
 else if (num3 > largest) {                                               // Check if 'num3' is greater than the current 'largest'
     largest = num3;                                                      // If true, assign 'num3' as the largest
-}
+`   }
 
 cout << "The largest number is: " << largest << endl;                    // Print the largest number after all checks
 
 return 0; 
+}
