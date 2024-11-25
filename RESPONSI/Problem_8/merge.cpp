8c7c9b44bd3304c45d6bc089a1672ec6c7ca889f
akht444r <akhtargadangabimanyu@mail.ugm.ac.id>
2024-11-25 16:50:31 +0700

    Update merge.cpp

--- RESPONSI/Problem_8/merge.cpp
+++ RESPONSI/Problem_8/merge.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+unsigned int data[3];
+unsigned string name[3];
+
+int main(){
+    cout << "Input the name of 3 students: "; cin >> name[3];
+    cout << "Input the score of 3 students: "; cin >> data[3];
+    
+    int temp, sorted = 0;
+    for (int i=1; i<=name[3]; i++){
+        temp = name[x];
+        cout << "Merged list: " << endl;
+    }
+}
